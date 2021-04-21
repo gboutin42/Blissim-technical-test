bd3fc1237261714ebedac63f7f6eba874589c873
gboutin <psyko_666@hotmail.fr>
2021-04-22 01:51:27 +0200

    form comment added

---
+++ php/app/Views/Comments/addComments.php
@@ -0,0 +1,6 @@
+<form id="formAddComment" method="post" action="" class="flex flex-col my-6 p-12 mx-12 rounded-xl bg-green-400 text-blue-600">
+	<input type="text" name="name" id="name" class="p-2 m-2" placeholder="Votre Nom">
+	<input type="email" name="email" id="email" class="p-2 m-2" placeholder="Votre Email">
+	<textarea name="comment" id="comment" cols="30" rows="10" class="p-2 m-2" placeholder="Ajouter votre commentaire ici..."></textarea>
+	<input id="btnAddComment" type="" class="bg-green-300 hover:bg-green-800 text-white font-bold py-2 px-4 rounded m-auto" value="Ajouter un commentaire">
+</form>
