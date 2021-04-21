e26694f428c052e65a9c7d5ce9aaaa7b51b5881f
gboutin <psyko_666@hotmail.fr>
2021-04-21 02:41:25 +0200

    header and footer added

---
+++ php/app/Views/Inc/footer.php
@@ -0,0 +1,7 @@
+    <footer class="bg-blue-600 text-red-50 text-center p-2">
+        <p>&copy; Copyright &middot; Boutin Grégory &middot; <span id="date-copyright"></span></p>
+    </footer>
+</body>
+</html>
+
+<script src="js/global.js"></script>
---
+++ php/app/Views/Inc/header.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="icons/logo-web.png">
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
+    <title>Test Technique Blissim</title>
+</head>
+<body>
+    <header class="bg-blue-600 text-red-50 text-center text-3xl p-6">
+        <h1>Test Technique Blissim &middot; E-Commerce</h1>
+    </header>
