56226d798eb72fc0610d2ff59f1d6b030e015740
gboutin <psyko_666@hotmail.fr>
2021-04-22 01:45:22 +0200

    include jquery and change path to favicon

--- php/app/Views/Inc/header.php
+++ php/app/Views/Inc/header.php
@@ -3,8 +3,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="resources/img/logo-web.png">
+    <link rel="icon" type="image/png" href="resources/icons/logo-web.png">
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>Test Technique Blissim</title>
 </head>
 <body>
