16193736a720c9faae92e069bb9d9c98ed91db6e
gboutin <psyko_666@hotmail.fr>
2021-04-23 01:56:40 +0200

    update index and create router's file

---
+++ php/public/app/router/router.php
@@ -0,0 +1,42 @@
+<?php
+use App\Controllers\ProductsController;
+use App\Controllers\CommentsController;
+use App\Models\Db;
+
+$router->map('GET', '/', function() {
+	header("Location: /products");
+});
+
+$router->map('GET', '/install', function() {
+	$db = new Db();
+	$db->create();
+	header("Refresh: 3;URL=../products");
+});
+
+$router->map('GET', '/products', function () {
+	new ProductsController();
+});
+
+$router->map('GET', '/product-[i:productId]', function ($productId) {
+	new ProductsController($productId);
+});
+
+$router->map('POST', '/add-comment-product-[i:productId]', function ($productId) {
+	$comment = new CommentsController();
+	return $comment->addComment($productId);
+});
+
+$router->map('POST', '/update-comment-product-[i:commentId]', function ($commentId) {
+	$comment = new CommentsController();
+	return $comment->updateComment($commentId);
+});
+
+$router->map('POST', '/delete-comment-product-[i:commentId]', function ($commentId) {
+	$comment = new CommentsController();
+	return $comment->deleteComment($commentId);
+});
+
+$router->map('POST', '/get-comments-product-[i:productId]', function ($productId) {
+	$comment = new CommentsController();
+	return $comment->getComments($productId);
+});
--- php/public/index.php
+++ php/public/index.php
@@ -1,33 +1,21 @@
 <?php
 require '../vendor/autoload.php';
-use App\Controllers\ProductsController;
 
 $router = new AltoRouter();
 
-$router->map('GET', '/', function() {
-	header("Location: /products");
-});
-
-$router->map('GET', '/install', function() {
-	require_once("../config/setup.php");
-	header("Refresh: 3;URL=../products");
-});
-
-$router->map('GET', '/products', function () {
-	new ProductsController();
-});
-
-$router->map('GET', '/product-[i:id]', function ($id) {
-	new ProductsController($id);
-});
-
+include_once("app/router/router.php");
 
 $match = $router->match();
 if ($match != false)
 {
-	require_once("../app/Views/Inc/header.php");
-	$match["target"]($match['params']['id'] ?? '');
-	require_once("../app/Views/Inc/footer.php");
+	if($_SERVER['REQUEST_METHOD'] === 'POST') {
+		$match["target"]($match['params']['productId'] ?? '');
+	}
+	else {
+		require_once("../app/Views/Inc/header.php");
+		$match["target"]($match['params']['productId'] ?? '');
+		require_once("../app/Views/Inc/footer.php");
+	}
 }
 else
 {
