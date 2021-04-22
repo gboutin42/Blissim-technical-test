a698b3d282f072d441faaf2c67118bdcb0d4b945
gboutin <psyko_666@hotmail.fr>
2021-04-23 01:54:35 +0200

    create model for db connection

--- php/app/Models/Db.php
+++ php/app/Models/Db.php
@@ -4,18 +4,64 @@ namespace App\Models;
 
 class Db
 {
+    private $variableDb;
     public $pdo;
-
+    
     public function __construct()
     {
-        require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'database.php';
+        $this->variableDb = dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'database.php';
+        require $this->variableDb;
 
         try {
-            $this->$pdo = new \PDO($DB_DSN, $DB_USER, $DB_PWD);
-            $this->$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->pdo = new \PDO($DB_DSN, $DB_USER, $DB_PWD);
+            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
         catch (\PDOException $error) {
             die("Error query " . $error->getMessage());
+        }
+    }
+
+    public function create()
+    {
+        require $this->variableDb;
+        
+        //CREATE DB
+        try {
+            $pdo = new \PDO($DB_DSN_LIGHT, $DB_USER, $DB_PWD);
+            $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+            $sql = $DB_DROP . " IF EXISTS " . $DB_NAME;
+            $pdo->exec($sql);
+            echo "Database DROPED successfully<br/>";
+            
+            $sql = $DB_CREATE . " IF NOT EXISTS " . $DB_NAME;
+            $pdo->exec($sql);
+            echo "Database CREATED successfully<br/>";
+        }
+        catch (\PDOException $error) {
+            die("ERROR CREATING DB: " . $error->getMessage() . "Aborting process");
+        }
+
+        //CREATE TABLE
+        try {
+            $pdo = new \PDO($DB_DSN, $DB_USER, $DB_PWD);
+            $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+            // $sql = $DB_TABLE_USERS;
+            // $pdo->exec($sql);
+            // echo "Table Users CREATED successfully.<br/>";
+
+            $sql = $DB_TABLE_PRODUCTS;
+            $pdo->exec($sql);
+            echo "Table Products CREATED successfully.<br/>";
+            
+            $sql = $DB_TABLE_COMMENTS;
+            $pdo->exec($sql);
+            echo "Table Comments CREATED successfully.<br/>";
+            
+        }
+        catch (\PDOException $error) {
+            die("ERROR CREATING TABLES: " . $error->getMessage() . "Aborting process");
         }
     }
 }
