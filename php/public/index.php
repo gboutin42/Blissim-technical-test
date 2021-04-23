eee81256471c6d3f71092a637297ab00583c3aad
gboutin <psyko_666@hotmail.fr>
2021-04-24 00:00:52 +0200

    secure way router in index

--- php/public/index.php
+++ php/public/index.php
@@ -8,12 +8,16 @@ include_once("app/router/router.php");
 $match = $router->match();
 if ($match != false)
 {
-	if($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$match["target"]($match['params']['productId'] ?? '');
+	if($_SERVER['REQUEST_METHOD'] === 'POST' || isset($_GET['api'])) {
+		$params = $match['params']['productId'] ??
+					$match['params']['commentId'] ?? '';
+		$match["target"]($params);
 	}
 	else {
 		require_once("../app/Views/Inc/header.php");
-		$match["target"]($match['params']['productId'] ?? '');
+		$params = $match['params']['productId'] ??
+					$match['params']['commentId'] ?? '';
+		$match["target"]($params);
 		require_once("../app/Views/Inc/footer.php");
 	}
 }
