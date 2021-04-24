4454b981d6c8955a1afa577be62c225cf23e3239
gboutin <psyko_666@hotmail.fr>
2021-04-24 13:07:38 +0200

    update database.php for clean and change variable

--- php/config/database.php
+++ php/config/database.php
@@ -9,27 +9,6 @@ $DB_PWD			= "my_db_password";
 $DB_CREATE		= "CREATE DATABASE ";
 $DB_DROP		= "DROP DATABASE ";
 
-// $DB_TABLE_USERS	=
-// 	"CREATE TABLE IF NOT EXISTS users
-// 	(
-// 		`id_user`			INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-// 		`firstname`			VARCHAR(50) NOT NULL,
-// 		`lastname`			VARCHAR(50) NOT NULL,
-// 		`mail`				VARCHAR(255) NOT NULL,
-// 		`password`			VARCHAR(255) NOT NULL,
-// 		`validate_account`	BOOLEAN NOT NULL DEFAULT '0'
-// 	);";
-
-$DB_TABLE_PRODUCTS	= 
-	"CREATE TABLE IF NOT EXISTS products (
-		`id_product`		INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-		`title`   			VARCHAR(255) NOT NULL,
-		`description`		TEXT COLLATE utf8mb4_general_ci NOT NULL,
-		`image_product_url`	VARCHAR(255) NOT NULL,
-		`category`	        VARCHAR(255) NOT NULL,
-		`price`				DECIMAL(12,2) NOT NULL
-	);";
-
 $DB_TABLE_COMMENTS	=
 	"CREATE TABLE IF NOT EXISTS comments
 	(
